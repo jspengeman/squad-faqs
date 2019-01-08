dbf28982132e2d494207d8bead47081ba8071f01
Jonathan <jonathan.spengeman@gmail.com>
2019-01-07 21:51:03 -0500

    Updated the header to include a title.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,31 +1,53 @@
 import React from 'react'
-import Logo from './Logo'
+import { StaticQuery, graphql } from 'gatsby'
 import styled from 'styled-components'
+import { Either, identity, value } from '../utils/functions'
+import Logo from './Logo'
 
 const Container = styled.section`
-  height: 80px;
   width: 80%;
   margin: 0 auto;
   margin-top: 10px;
-
-  @media (min-width: 456px) {
-    height: 130px;
-  }
-  @media (min-width: 564px) {
-    height: 140px;
-  }
-  @media (min-width: 768px) {
-    height: 150px;
-  }
+  margin-bottom: 50px;
 `
+
+const Title = styled.h4`
+  text-align: center;
+  margin-top: 5px;
+`
+
+const FancyTitle = () => (
+  <StaticQuery
+    query={graphql`
+      query siteTitle {
+        site {
+          siteMetadata {
+            description
+          }
+        }
+      }
+    `}
+    render={data => (
+      <Title>
+        {Either.fromPath(['site', 'siteMetadata', 'description'])(data).fold(
+          value(''),
+          identity
+        )}
+      </Title>
+    )}
+  />
+)
 
 const Header = () => {
   return (
-    <Container>
-      <a href="http://joinsquad.com/">
-        <Logo />
-      </a>
-    </Container>
+    <div>
+      <Container>
+        <a href="http://joinsquad.com/">
+          <Logo />
+        </a>
+        <FancyTitle>Community currated frequently asked questions.</FancyTitle>
+      </Container>
+    </div>
   )
 }
 
