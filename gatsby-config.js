7469207e43bed674354cc2a0523aab9886525da7
Jonathan <jonathan.spengeman@gmail.com>
2019-01-07 20:17:28 -0500

    Added offline support via a service worker.

--- gatsby-config.js
+++ gatsby-config.js
@@ -52,8 +52,6 @@ module.exports = {
       },
     },
     `gatsby-plugin-netlify`,
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.app/offline
-    // 'gatsby-plugin-offline',
+    'gatsby-plugin-offline',
   ],
 }
